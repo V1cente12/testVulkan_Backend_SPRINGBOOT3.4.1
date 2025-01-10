7d12f438ff9f269054f0fe38f87fe1f8ce8dae75
V1cente12 <jvicentecv@gmail.com>
2025-01-09 21:00:25 -0400

    course student service

---
+++ src/main/java/com/example/testVulkan_Backend_SPRINGBOOT341/service/CourseService.java
@@ -0,0 +1,32 @@
+package com.example.testVulkan_Backend_SPRINGBOOT341.service;
+
+import com.example.testVulkan_Backend_SPRINGBOOT341.model.Course;
+import com.example.testVulkan_Backend_SPRINGBOOT341.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Course saveCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public Course getCourse(Long id) {
+        return courseRepository.findById(id).orElse(null);
+    }
+
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/testVulkan_Backend_SPRINGBOOT341/service/StudentService.java
@@ -0,0 +1,32 @@
+package com.example.testVulkan_Backend_SPRINGBOOT341.service;
+
+import com.example.testVulkan_Backend_SPRINGBOOT341.model.Student;
+import com.example.testVulkan_Backend_SPRINGBOOT341.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student getStudent(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
