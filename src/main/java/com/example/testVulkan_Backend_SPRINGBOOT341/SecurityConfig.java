03b5b577c18f03319dc8ad1b4a520ec4bb15de7d
V1cente12 <jvicentecv@gmail.com>
2025-01-10 00:20:25 -0400

    config spring security

---
+++ src/main/java/com/example/testVulkan_Backend_SPRINGBOOT341/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.testVulkan_Backend_SPRINGBOOT341;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/course/**").authenticated()
+                        .requestMatchers("/student/**").authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService testUser(PasswordEncoder passwordEncoder) {
+        UserDetails user1 = User.builder()
+                .username("vicente")
+                .password(passwordEncoder.encode("abc123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
