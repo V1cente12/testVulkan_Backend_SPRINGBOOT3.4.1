d1161c6cec778eaa59d4a79c561df1656ef991a5
V1cente12 <jvicentecv@gmail.com>
2025-01-10 00:19:32 -0400

    fix routes

--- src/main/java/com/example/testVulkan_Backend_SPRINGBOOT341/controller/CourseController.java
+++ src/main/java/com/example/testVulkan_Backend_SPRINGBOOT341/controller/CourseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/course")
 public class CourseController {
 
     @Autowired
