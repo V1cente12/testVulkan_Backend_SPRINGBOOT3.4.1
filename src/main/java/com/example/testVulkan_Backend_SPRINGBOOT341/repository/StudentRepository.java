7a60ca40d4558c36846605f05980299ec889005b
V1cente12 <jvicentecv@gmail.com>
2025-01-11 23:32:18 -0400

    update and save student validations

--- src/main/java/com/example/testVulkan_Backend_SPRINGBOOT341/repository/StudentRepository.java
+++ src/main/java/com/example/testVulkan_Backend_SPRINGBOOT341/repository/StudentRepository.java
@@ -4,7 +4,11 @@ import com.example.testVulkan_Backend_SPRINGBOOT341.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    long countByCourseId(Long courseId);
+    List<Student> findByCourseId(Long courseId);
 }
--- src/main/java/com/example/testVulkan_Backend_SPRINGBOOT341/service/StudentService.java
+++ src/main/java/com/example/testVulkan_Backend_SPRINGBOOT341/service/StudentService.java
@@ -1,7 +1,9 @@
 package com.example.testVulkan_Backend_SPRINGBOOT341.service;
 
+import com.example.testVulkan_Backend_SPRINGBOOT341.model.Course;
 import com.example.testVulkan_Backend_SPRINGBOOT341.model.Student;
 import com.example.testVulkan_Backend_SPRINGBOOT341.repository.StudentRepository;
+import com.example.testVulkan_Backend_SPRINGBOOT341.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +15,27 @@ public class StudentService {
 
     @Autowired
     private StudentRepository studentRepository;
+    @Autowired
+    private CourseRepository courseRepository;
 
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
 
     public Student saveStudent(Student student) {
+        Course course = courseRepository.findById(student.getCourse().getId())
+                .orElseThrow(() -> new RuntimeException("Course not found"));
+
+        long enrolledCount = studentRepository.countByCourseId(course.getId());
+        if (enrolledCount >= course.getMaxCapacity()) {
+            throw new RuntimeException("The course has reached its maximum capacity");
+        }
+
+        student.setCourse(course);
+
         return studentRepository.save(student);
     }
+
 
     public Student getStudent(Long id) {
         return studentRepository.findById(id).orElse(null);
@@ -28,5 +43,27 @@ public class StudentService {
 
     public void deleteStudent(Long id) {
         studentRepository.deleteById(id);
+    }
+
+    public Student updateStudent(Long id, Student student) {
+        Student existingStudent = studentRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Student not found")
+        );
+
+        Course course = courseRepository.findById(student.getCourse().getId())
+                .orElseThrow(() -> new RuntimeException("Course not found"));
+
+
+        long enrolledCount = studentRepository.countByCourseId(course.getId());
+        if (enrolledCount >= course.getMaxCapacity()) {
+            throw new RuntimeException("The course has reached its maximum capacity");
+        }
+
+        existingStudent.setName(student.getName());
+        existingStudent.setAge(student.getAge());
+        existingStudent.setGender(student.getGender());
+        existingStudent.setCourse(course);
+
+        return studentRepository.save(existingStudent);
     }
 }
